eaf053a9073b4fe0a919705d6b3bf456fb2fe74d
Elena <lenarusskih@gmail.com>
2023-08-30 07:52:50 +0300

    Initial commit

---
+++ PasscodeView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PasscodeView
+//
+//  Created by Елена Русских on 2023-08-30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
