47f2feab6d1fd6dc8ecdfac1b91865f8668fe3f4
Elena <lenarusskih@gmail.com>
2023-09-01 11:34:17 +0300

    Add unit tests for PasscodeRouter file

--- PasscodeViewTests/Mocks/MockNavigationController.swift
+++ PasscodeViewTests/Mocks/MockNavigationController.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+import UIKit
+
+class MockNavigationController: UINavigationController {
+    var didPushViewController = false
+    var pushedViewController: UIViewController?
+    override func pushViewController(_ viewController: UIViewController, animated: Bool) {
+        didPushViewController = true
+        pushedViewController = viewController
+    }
+}
--- PasscodeViewTests/PasscodeRouterTests.swift
+++ PasscodeViewTests/PasscodeRouterTests.swift
@@ -5,4 +5,34 @@
 //  Created by Елена Русских on 2023-09-01.
 //
 
-import Foundation
+import XCTest
+@testable import PasscodeView
+
+final class PasscodeRouterTests: XCTestCase {
+    
+    var router: PasscodeRouter!
+    var mockNavController: MockNavigationController!
+
+    override func setUpWithError() throws {
+        mockNavController = MockNavigationController()
+        router = PasscodeRouter(navigationController: mockNavController)
+    }
+
+    override func tearDownWithError() throws {
+        router = nil
+        mockNavController = nil
+    }
+
+    func test_initializationWithNavController() {
+        //Then
+        XCTAssertNotNil(router.navigationController, "After initialization, the router's navigationController should not be nil.")
+    }
+
+    func test_navigateToNextScreen() {
+        //When
+        router.navigateToNextScreen()
+        //Then
+        XCTAssertTrue(mockNavController.didPushViewController, "After calling navigateToNextScreen, the pushViewController method should be invoked.")
+        XCTAssertTrue(mockNavController.pushedViewController is SecondViewController, "The passed controller should be an instance of SecondViewController.")
+    }
+}
