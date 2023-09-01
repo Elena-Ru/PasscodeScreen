c5a28828cfbe413679a3688011a2c1841cb6bd68
Elena <lenarusskih@gmail.com>
2023-09-01 11:33:51 +0300

    Add unit tests for PasscodeRouter

---
+++ PasscodeViewTests/Mocks/MockNavigationController.swift
@@ -0,0 +1,8 @@
+//
+//  MockNavigationController.swift
+//  PasscodeViewTests
+//
+//  Created by Елена Русских on 2023-09-01.
+//
+
+import Foundation
---
+++ PasscodeViewTests/PasscodeRouterTests.swift
@@ -0,0 +1,8 @@
+//
+//  PasscodeRouterTests.swift
+//  PasscodeViewTests
+//
+//  Created by Елена Русских on 2023-09-01.
+//
+
+import Foundation
